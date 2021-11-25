97866e3fd481d96cbabc399fbab33318405db576
HeptaDecane <sooraj1999vs@gmail.com>
2021-11-26 01:39:45 +0530

    vector_add

---
+++ CudaSandbox/vector_add.cu
@@ -0,0 +1,24 @@
+#define N 10000000
+
+void vector_add(float *out, float *a, float *b, int n) {
+    for(int i = 0; i < n; i++){
+        out[i] = a[i] + b[i];
+    }
+}
+
+int main(){
+    float *a, *b, *out;
+
+    // Allocate memory
+    a   = new float[N];
+    b   = new float[N];
+    out = new float[N];
+
+    // Initialize array
+    for(int i = 0; i < N; i++){
+        a[i] = 1.0f; b[i] = 2.0f;
+    }
+
+    // Main function
+    vector_add(out, a, b, N);
+}
