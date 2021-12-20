733cf38b758470725d59e0953c04ac2f9a3f6f03
HeptaDecane <sooraj1999vs@gmail.com>
2021-12-20 20:52:46 +0530

    improved

--- A04/best_first_search.cpp
+++ A04/best_first_search.cpp
@@ -1,57 +1,73 @@
-#include<iostream>
-#include<omp.h>
-
+#include<bits/stdc++.h>
+#include "omp.h"
 using namespace std;
-int q[100];
-int visited[7];
-int local_q;
 
-void bfs(int adj_matrix[7][7], int first, int last, int q[], int n_nodes) {
-    if(first==last)
-      return;
-    
-    int cur_node = q[first++];
-    cout<<"  "<<cur_node;
-    
-    omp_set_num_threads(3);
-    
-    #pragma omp parallel for shared(visited)
-    for(int i=0; i<n_nodes; i++) {
-        if(adj_matrix[cur_node][i] == 1 && visited[i] == 0){
-            q[last++] = i;
-            visited[i] = 1;
+#define N 7
+
+list<int> q;
+vector<int> weight(N, 1000);
+bool *visited = new bool[N]{false};
+
+struct Comparator {
+    // Compare 2 Edges objects using weight
+    bool operator ()(const int &e1, const int &e2){
+        return weight[e1]<weight[e2];
+    }
+};
+
+void best_first_search(int adj_matrix[N][N])
+{
+    if(q.empty()) return;
+    q.sort(Comparator());
+
+    //pop first element and display it
+    int current_node = q.front();
+    q.pop_front();
+    cout<<current_node<<", ";
+
+    //For every element in the row of the adjacency matrix
+    #pragma omp parallel for shared(visited,q,weight)
+    for(int i=0; i<N; i++){
+        //If an unvisited Edge exists
+        if(adj_matrix[current_node][i] > 0 && !visited[i]){
+
+            //Replace the weight if it is larger
+            if(weight[i] > adj_matrix[current_node][i])
+                weight[i] = adj_matrix[current_node][i];
+
+            //Push the destination of the smallest edge onto the queue
+            q.push_back(i);
+            visited[i]=true;
         }
     }
-    
-    bfs(adj_matrix, first, last, q, n_nodes);
+
+    //Call the function recursively
+    best_first_search(adj_matrix);
 }
 
-int main() {
-    int first = -1;
-    int last = 0;
-    int n_nodes = 7;
-    
-    for(int i=0; i<n_nodes; i++) {
-        visited[i] = 0;
-    }
-    
-    int adj_matrix[7][7] = {
-      {0, 1, 1, 0, 0, 0, 0},
-      {1, 0, 1, 1, 0, 0, 0},
-      {1, 1, 0, 0, 1, 0, 0},
-      {0, 1, 0, 0, 1, 0, 0},
-      {0, 0, 1, 1, 0, 1, 0},
-      {0, 0, 0, 0, 1, 0, 1},
-      {0, 0, 0, 0, 0, 1, 0}
+int main(){
+    //Set the Adjacency Matrix
+    int adj_matrix[N][N] = {
+            {0	,10  ,15  ,0  ,0  ,0  ,0},
+            {10	,0	,25	,25	,0	,0	,0},
+            {15	,25	,0	,0	,40	,0	,0},
+            {0	,25	,0	,0	,20	,0	,0},
+            {0	,0	,40	,20	,0	,5	,0},
+            {0	,0	,0	,0	,5	,0	,20},
+            {0	,0	,0	,0	,0	,20	,0}
     };
-    
+
     int start_node = 3;
-    q[last++] = start_node;
-    first++;
-    visited[start_node] = 1;
- 
-    bfs(adj_matrix, first, last, q, n_nodes);
-    
+    q.push_back(start_node);
+    weight[start_node] = 0;
+    visited[start_node] = true;
+
+    omp_set_num_threads(8);
+    double start = omp_get_wtime();
+    best_first_search(adj_matrix);
+    double end = omp_get_wtime();
+    cout<<endl;
+    cout<<"time: "<<(end-start)<<endl;
+
     return 0;
-    
 }
--- A04/binary_search.cpp
+++ A04/binary_search.cpp
@@ -1,63 +1,57 @@
-#include<iostream>
-
-#include<omp.h>
-
-#include<time.h>
-
+#include "bits/stdc++.h"
+#include "omp.h"
 using namespace std;
-int c1[3];
-int binary_search(int a[], int low, int high, int key) {
+
+#define N 1000000
+
+int binary_search(const int *a, int low, int high, int key) {
     int loc = -1;
+    int current_thead;
     int mid;
+
     while (low <= high) {
-        mid = (high + low) / 2;
+        mid = (high+low)/2;
         if (a[mid] == key) {
             loc = mid;
+            cout<<"found by thread: "<<current_thead<<endl;
             break;
-        } 
+        }
         else {
-            #pragma omp parallel sections 
+            #pragma omp parallel sections
             {
-                //cout<<omp_get_thread_num();
-                #pragma omp section 
+                #pragma omp section
                 {
-                    if (a[mid] < key) {
-                        low = mid + 1;
-                    }
+                    if (a[mid] < key) low = mid + 1;
+                    current_thead = omp_get_thread_num();
                 }
-                #pragma omp section 
+                #pragma omp section
                 {
-                    if (a[mid] > key) {
-                        high = mid - 1;
-                    }
+                    if (a[mid] > key) high = mid - 1;
+                    current_thead = omp_get_thread_num();
                 }
             }
         }
     }
+
     return loc;
 }
+
 int main() {
-    int th = omp_get_max_threads();
-    cout << "Max Threads : " << th << endl;
-    omp_set_num_threads(2);
-    c1[1] = 0;
-    c1[2] = 0;
-    int a[1000000];
-    clock_t t1, t2;
-    int key = 0;
-    int loc, i;
-    for (int i = 0; i < 1000000; i++) {
-        a[i] = i;
-    }
-    key = 1000;
-    t1 = clock();
-    loc = binary_search(a, 0, 1000000, key);
-    t2 = clock();
-    if (loc == -1) {
-        cout << "Not Found";
-    } else {
-        cout << "Found at " << loc << endl;
-        //cout<<"By Thread "<<omp_get_thread_num()<<endl;
-    }
+
+    int *a = new int[N];
+    int offset = rand()%97;
+    for(int i=0; i<N; i++) a[i]=i+offset;
+
+    int key = 69420;
+
+    omp_set_num_threads(8);
+    double start = omp_get_wtime();
+    int loc = binary_search(a, 0, N, key);
+    double end = omp_get_wtime();
+
+    if (loc==-1) cout<<key<<" not found"<<endl;
+    else cout<<key<<" found at index: "<<loc<<endl;
+    cout<<"time: "<<(end-start)<<endl;
+
     return 0;
 }
